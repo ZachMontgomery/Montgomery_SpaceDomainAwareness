52b5298f4dfb1a927cc275a6e08990ecf638ce98
unknown <zachary.s.montgomery@gmail.com>
2020-10-29 01:29:06 -0600

    fixed merging errors on +tdoa

--- +tdoa/compute_residual.m
+++ +tdoa/compute_residual.m
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 function res = compute_residual( ztilde, ztildehat )
 %compute_residual_example calculates the measurement residual
 res = ztilde - ztildehat;
-=======
-function res = compute_residual( ztilde, ztildehat )
-%compute_residual_example calculates the measurement residual
-res = ztilde - ztildehat;
->>>>>>> b18674797ffbdbe70d78574a39a954e3c6a714c2
 end
