eb75e55b570601ce6d9ba4fe98ef0f0bbf62e99d
Oll Jansson <olli.jansson@aggiemail.usu.edu>
2021-08-04 16:39:46 -0600

    changed calc_B

--- calc_B.m
+++ calc_B.m
@@ -1,10 +1,10 @@
 function B = calc_B( simpar )
 
 Na = simpar.general.n_assets;
-Nw = Na + 6;
+Nw = 3*Na + 6;
 
-B = [eye(Na), zeros(Na,6);
+B = [eye(3*Na), zeros(3*Na,6);
     zeros(3,Nw);
-    zeros(6,Na), eye(6)];
+    zeros(6,3*Na), eye(6)];
 
 end
